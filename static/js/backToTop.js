19e2bfbed09807628cd06c14ba27585227073556
Rémy Jacquin <remy@remyj.fr>
2017-09-23 21:40:35 +0200

    Fix CSS/JS files identation

--- static/js/backToTop.js
+++ static/js/backToTop.js
@@ -1,16 +1,16 @@
-$(function(){
+$(function () {
     // browser window scroll (in pixels) after which the "back to top" link is shown
     var offset = 300,
-        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-        offset_opacity = 1200,
-        //duration of the top scrolling animation (in ms)
-        scroll_top_duration = 700,
-        //grab the "back to top" link
-        $backToTop = $('#back-to-top');
-        $shareNav = $('#share-nav');
+            //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+            offset_opacity = 1200,
+            //duration of the top scrolling animation (in ms)
+            scroll_top_duration = 700,
+            //grab the "back to top" link
+            $backToTop = $('#back-to-top');
+    $shareNav = $('#share-nav');
 
     //hide or show the "back to top" link
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         if ($(this).scrollTop() > offset) {
             $backToTop.fadeIn();
             $shareNav.fadeIn();
@@ -20,18 +20,18 @@ $(function(){
             $shareNav.fadeOut();
         }
 
-        if($(this).scrollTop() > offset_opacity) {
-			$backToTop.addClass('btt-fade-out');
-		}
+        if ($(this).scrollTop() > offset_opacity) {
+            $backToTop.addClass('btt-fade-out');
+        }
     });
 
     //smooth scroll to top
-    $backToTop.on('click', function(event){
+    $backToTop.on('click', function (event) {
         event.preventDefault();
         $('body,html').animate({
-            scrollTop: 0 ,
-             }, scroll_top_duration
-        );
+            scrollTop: 0,
+        }, scroll_top_duration
+                );
     });
 
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,168 +1,168 @@
 /*
-	Future Imperfect by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+ Future Imperfect by HTML5 UP
+ html5up.net | @n33co
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
 
-(function($) {
+(function ($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)'
+    });
 
-	$(function() {
+    $(function () {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$menu = $('#menu'),
-			$shareMenu = $('#share-menu'),
-			$sidebar = $('#sidebar'),
-			$main = $('#main'),
-			$translationsMenu = $('#translations-menu');
+        var $window = $(window),
+                $body = $('body'),
+                $menu = $('#menu'),
+                $shareMenu = $('#share-menu'),
+                $sidebar = $('#sidebar'),
+                $main = $('#main'),
+                $translationsMenu = $('#translations-menu');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        $window.on('load', function () {
+            window.setTimeout(function () {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function () {
+            $.prioritize(
+                    '.important\\28 medium\\29',
+                    skel.breakpoint('medium').active
+                    );
+        });
 
-		// IE<=9: Reverse order of main and sidebar.
-			if (skel.vars.IEVersion <= 9)
-				$main.insertAfter($sidebar);
+        // IE<=9: Reverse order of main and sidebar.
+        if (skel.vars.IEVersion <= 9)
+            $main.insertAfter($sidebar);
 
-		$menu.appendTo($body);
-		$shareMenu.appendTo($body);
-		$translationsMenu.appendTo($body);
+        $menu.appendTo($body);
+        $shareMenu.appendTo($body);
+        $translationsMenu.appendTo($body);
 
-		$menu.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnEscape: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'right',
-			target: $body,
-			visibleClass: 'is-menu-visible'
-		});
+        $menu.panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnEscape: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'right',
+            target: $body,
+            visibleClass: 'is-menu-visible'
+        });
 
-		$shareMenu.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnEscape: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'right',
-			target: $body,
-			visibleClass: 'is-share-visible'
-		});
+        $shareMenu.panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnEscape: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'right',
+            target: $body,
+            visibleClass: 'is-share-visible'
+        });
 
-		$translationsMenu.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnEscape: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'top',
-			target: $body,
-			visibleClass: 'is-translations-visible'
-		});
-		// Menu.
-			/*$menu
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-menu-visible'
-				});*/
+        $translationsMenu.panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnEscape: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'top',
+            target: $body,
+            visibleClass: 'is-translations-visible'
+        });
+        // Menu.
+        /*$menu
+         .appendTo($body)
+         .panel({
+         delay: 500,
+         hideOnClick: true,
+         hideOnSwipe: true,
+         resetScroll: true,
+         resetForms: true,
+         side: 'right',
+         target: $body,
+         visibleClass: 'is-menu-visible'
+         });*/
 
-		// Search (header).
-			var $search = $('#search'),
-				$search_input = $search.find('input');
+        // Search (header).
+        var $search = $('#search'),
+                $search_input = $search.find('input');
 
-			$body
-				.on('click', '[href="#search"]', function(event) {
+        $body
+                .on('click', '[href="#search"]', function (event) {
 
-					event.preventDefault();
+                    event.preventDefault();
 
-					// Not visible?
-						if (!$search.hasClass('visible')) {
+                    // Not visible?
+                    if (!$search.hasClass('visible')) {
 
-							// Reset form.
-								$search[0].reset();
+                        // Reset form.
+                        $search[0].reset();
 
-							// Show.
-								$search.addClass('visible');
+                        // Show.
+                        $search.addClass('visible');
 
-							// Focus input.
-								$search_input.focus();
+                        // Focus input.
+                        $search_input.focus();
 
-						}
+                    }
 
-				});
+                });
 
-			$search_input
-				.on('keydown', function(event) {
+        $search_input
+                .on('keydown', function (event) {
 
-					if (event.keyCode == 27)
-						$search_input.blur();
+                    if (event.keyCode == 27)
+                        $search_input.blur();
 
-				})
-				.on('blur', function() {
-					window.setTimeout(function() {
-						$search.removeClass('visible');
-					}, 100);
-				});
+                })
+                .on('blur', function () {
+                    window.setTimeout(function () {
+                        $search.removeClass('visible');
+                    }, 100);
+                });
 
-		// Intro.
-			var $intro = $('#intro');
+        // Intro.
+        var $intro = $('#intro');
 
-			// Move to main on <=large, back to sidebar on >large.
-				skel
-					.on('+large', function() {
-						$intro.prependTo($main);
-					})
-					.on('-large', function() {
-						$intro.prependTo($sidebar);
-					});
+        // Move to main on <=large, back to sidebar on >large.
+        skel
+                .on('+large', function () {
+                    $intro.prependTo($main);
+                })
+                .on('-large', function () {
+                    $intro.prependTo($sidebar);
+                });
 
-			// Translations menu
-			//$translationsMenu = $('#wrapper .translations-menu');
-			//$body
-			//	.on('click', '[href="#translations-menu"]', function(event) {
-			//		event.preventDefault();
-			//		// Not visible?
-			//		if (!$translationsMenu.is(':visible')) {
-			//			$translationsMenu.show();
-			//		} else {
-			//			$translationsMenu.hide()
-				//	}
-			//	});
-	});
+        // Translations menu
+        //$translationsMenu = $('#wrapper .translations-menu');
+        //$body
+        //	.on('click', '[href="#translations-menu"]', function(event) {
+        //		event.preventDefault();
+        //		// Not visible?
+        //		if (!$translationsMenu.is(':visible')) {
+        //			$translationsMenu.show();
+        //		} else {
+        //			$translationsMenu.hide()
+        //	}
+        //	});
+    });
 
 })(jQuery);
